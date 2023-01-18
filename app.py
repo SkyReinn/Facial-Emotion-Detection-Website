baca18ce95786426c9605b3a878d655c23ada718
SkyReinn <trudersunnyx@gmail.com>
2023-01-18 08:08:48 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ Created on Mon Jan  9 22:49:37 2023
 
 from flask import Flask, request, render_template
 from keras.models import load_model
-import matplotlib.pyplot as plt 
 from PIL import Image
 import numpy as np
 
@@ -35,10 +34,6 @@ def predict():
     #save grayscale image to variable
     img_array = np.array(img)
     
-    #show image in 48*48 greyscale format
-    plt.imshow(img_array, cmap='gray')
-    plt.show()
-    
     #reshape image for prediction
     img_array = np.expand_dims(img_array, axis=-1)
     img_array = np.stack((img_array,)*3, axis=-1)
@@ -46,8 +41,6 @@ def predict():
 
     #preciction
     pred = vgg.predict(img_array)[0]
-    print("prediction:")
-    print(pred)
     emotions = ["Angry","Happy","Sad","Surprise","Neutral"]
     pred_emotion = emotions[np.argmax(pred)]
     
